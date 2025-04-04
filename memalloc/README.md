0f9defd48125376147b07876c21dc38bc8468ea4
ccryssia <gshine@m.scnu.edu.cn>
2025-04-04 21:06:23 +0800

    Using DFS to collect reachable chunks in heap

--- memalloc/README.md
+++ memalloc/README.md
@@ -1,6 +1,6 @@
 # memalloc
 
-memalloc is a simple allocator which implemented [linear allocating](https://en.wikipedia.org/wiki/C_dynamic_memory_allocation#Heap-based) with fixed size chunks.
+memalloc is a simple allocator which implemented [linear allocating](https://en.wikipedia.org/wiki/C_dynamic_memory_allocation#Heap-based) with fixed size chunks. It only supports to collect aligned address in the heap and stack.
 
 ## Quick start
 
@@ -13,3 +13,4 @@ $ ./memalloc
 
 - Tsoding: [memalloc](https://github.com/tsoding/memalloc/)
 - JSON Serialization Library in C: https://github.com/tsoding/jim
+- IBM: [Data alignment: Straighten up and fly right](https://developer.ibm.com/articles/pa-dalign/)
--- memalloc/memalloc.c
+++ memalloc/memalloc.c
@@ -1,10 +1,13 @@
 #include "memalloc.h"
 #include <assert.h>
-#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 uintptr_t heap[HEAP_CAPACITY_WORDS] = {0};
+const uintptr_t *stack_base = NULL;
+bool reachable_chunks[CHUNK_LIST_CAPACITY] = {false};
 
 Chunk_List alloced_chunks = {0};
 Chunk_List freed_chunks = {
@@ -119,7 +122,7 @@ Node *generate_tree(size_t level_cur, size_t level_max)
     return node;
 }
 
-void print_tree(Node *root, Jim *jim)
+static void print_tree_recur(Node *root, Jim *jim)
 {
     if (root) {
         jim_object_begin(jim);
@@ -127,13 +130,24 @@ void print_tree(Node *root, Jim *jim)
         jim_member_key(jim, "value");
         jim_string_sized(jim, &root->x, 1);
         jim_member_key(jim, "left");
-        print_tree(root->left, jim);
+        print_tree_recur(root->left, jim);
         jim_member_key(jim, "right");
-        print_tree(root->right, jim);
+        print_tree_recur(root->right, jim);
 
         jim_object_end(jim);
     } else {
         jim_null(jim);
+    }
+}
+
+void print_tree(Node *root, Jim *jim)
+{
+    if (root) {
+        print_tree_recur(root, jim);
+    } else {
+        jim_object_begin(jim);
+        jim_null(jim);
+        jim_object_end(jim);
     }
 }
 
@@ -185,59 +199,85 @@ void heap_free(void *ptr)
     chunk_list_insert(&freed_chunks, chunk.addr, chunk.size);
 }
 
+// using DFS to collect reachable chunks in heap
+static void mark_region(const uintptr_t *start, const uintptr_t *end)
+{
+    for (; start <= end; start++) {
+        const uintptr_t *p = (uintptr_t *) (*start);
+        for (size_t i = 0; i < alloced_chunks.count; i++) {
+            const Chunk *chunk = &alloced_chunks.chunks[i];
+            if (p >= (uintptr_t *) chunk->addr &&
+                p < (uintptr_t *) chunk->addr + chunk->size &&
+                !reachable_chunks[i]) {
+                reachable_chunks[i] = true;
+                mark_region((uintptr_t *) chunk->addr,
+                            (uintptr_t *) chunk->addr + chunk->size);
+            }
+        }
+    }
+}
+
 void heap_collect(void)
 {
-    unimplemented();
+    const uintptr_t *stack_start = __builtin_frame_address(0);
+    memset(reachable_chunks, 0, sizeof(reachable_chunks));
+
+    // mark all reachable chunks recursively
+    mark_region(stack_start, stack_base);
+
+    const Chunk_List *chunk_list = &alloced_chunks;
+    printf("chunks (#%zu)\n", chunk_list->count);
+    for (size_t i = 0; i < chunk_list->count; i++) {
+        printf("  addr: %p\t", chunk_list->chunks[i].addr);
+        printf("  size: %zu\t", chunk_list->chunks[i].size);
+        printf("  reachable: %s\n", reachable_chunks[i] ? "true" : "false");
+    }
+
+    // remove all unreachable chunks
 }
 
 int main(void)
 {
-    // for (size_t i = 0; i < 20; i++) {
-    //     void *root = heap_alloc(i);
-    //     if (i % 2 == 0) {
-    //         heap_free(root);
+    stack_base = (uintptr_t *) __builtin_frame_address(0);
+    assert((uintptr_t) stack_base % sizeof(uintptr_t) == 0);
+
+    size_t level = 3;
+    Node *root = generate_tree(0, level);
+    printf("%p\n", (void *) root);
+    Jim jim = {
+        .sink = stdout,
+        .write = (Jim_Write) fwrite,
+    };
+    print_tree(root, &jim);
+
+    printf("\n--------------------------\n");
+
+    heap_collect();
+
+    printf("\n--------------------------\n");
+
+    root = NULL;
+    heap_collect();
+
+    // size_t heap_ptrs_count = 0;
+    // for (size_t i = 0; i < alloced_chunks.count; i++) {
+    //     for (size_t j = 0; j < alloced_chunks.chunks[i].size; j++) {
+    //         void *p = (void *) ((uintptr_t *)
+    //         alloced_chunks.chunks[i].addr)[j]; if (p >= (void *) heap &&
+    //             p < (void *) (heap + HEAP_CAPACITY_WORDS)) {
+    //             printf("DETECTED HEAP POINTER: %p\n", p);
+    //             heap_ptrs_count++;
+    //         }
     //     }
     // }
-
-    // heap_alloc(10);
+    // // heap_ptrs_count == 2^n - 2 which n is the tree's level
+    // assert(heap_ptrs_count == (size_t) (1 << level) - 2);
+    // printf("Detected %zu heap pointers\n", heap_ptrs_count);
 
     // printf("Heap alloced:\n");
     // chunk_list_dump(&alloced_chunks);
     // printf("Heap freed:\n");
     // chunk_list_dump(&freed_chunks);
-
-    // heap_collect();
-
-    size_t level = 5;
-    Node *tree = generate_tree(0, level);
-    Jim jim = {
-        .sink = stdout,
-        .write = (Jim_Write) fwrite,
-    };
-    assert(tree);
-    print_tree(tree, &jim);
-
-    printf("--------------------------\n");
-
-    size_t heap_ptrs_count = 0;
-    for (size_t i = 0; i < alloced_chunks.count; i++) {
-        for (size_t j = 0; j < alloced_chunks.chunks[i].size; j++) {
-            void *p = (void *) ((uintptr_t *) alloced_chunks.chunks[i].addr)[j];
-            if (p >= (void *) heap &&
-                p <= (void *) (heap + HEAP_CAPACITY_WORDS)) {
-                printf("DETECTED HEAP POINTER: %p\n", p);
-                heap_ptrs_count++;
-            }
-        }
-    }
-    // heap_ptrs_count == 2^n - 2 which n is the tree's level
-    assert(heap_ptrs_count == (size_t) (1 << level) - 2);
-    printf("Detected %zu heap pointers\n", heap_ptrs_count);
-
-    printf("Heap alloced:\n");
-    chunk_list_dump(&alloced_chunks);
-    printf("Heap freed:\n");
-    chunk_list_dump(&freed_chunks);
 
     return 0;
 }
